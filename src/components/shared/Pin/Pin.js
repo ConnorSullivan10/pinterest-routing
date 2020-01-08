d492dafd5d0a07818cb429381dcdffc8e56d88cf
Connor Sullivan <cksullvn@gmail.com>
2020-01-07 21:28:47 -0600

    delete pins works

--- src/components/shared/Pin/Pin.js
+++ src/components/shared/Pin/Pin.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import pinShape from '../../../helpers/props/pinShape';
 
 import './Pin.scss';
 
 class Pin extends React.Component {
+  static propTypes = {
+    pin: pinShape.pinShape,
+    deletePin: PropTypes.func,
+  }
+
+  deletePinEvent = (e) => {
+    e.preventDefault();
+    const { deletePin, pin } = this.props;
+    deletePin(pin.id);
+  }
+
   render() {
     const { pin } = this.props;
     return (
@@ -11,6 +24,7 @@ class Pin extends React.Component {
           <img src={pin.imageUrl} className="card-img-top" alt=""/>
           <div className="card-body">
             <h5 className="card-title">{pin.title}</h5>
+            <button className="btn btn-danger" onClick={this.deletePinEvent}>X</button>
           </div>
         </div>
       </div>
