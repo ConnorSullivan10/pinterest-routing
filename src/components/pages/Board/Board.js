a3b18cc946a44a5b67515de14e44356bb7c86f16
Connor Sullivan <cksullvn@gmail.com>
2020-01-07 19:51:13 -0600

    displaying boards

---
+++ src/components/pages/Board/Board.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './Board.scss';
+
+class Board extends React.Component {
+  render() {
+    const { board } = this.props;
+    return (
+      <div className="Board col-4">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">{board.name}</h5>
+            <p className="card-text">{board.description}</p>
+            <button className="btn btn-primary" onClick={this.setSelectedBoardId}>View Pins</button>
+            <button className="btn btn-danger" onClick={this.setEditMode}>Edit Board</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Board;
